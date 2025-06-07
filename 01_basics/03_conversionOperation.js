c95f7b298781799ce96b3eebf7a68d471e0749c9
vineet8057 <vineetkumar02102005@gmail.com>
2025-06-07 08:29:32 +0000

    Conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
