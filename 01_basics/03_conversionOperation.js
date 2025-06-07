b94908a15fc8810845a07d1d3bd933d6184655fe
vineet8057 <vineetkumar02102005@gmail.com>
2025-06-07 09:19:11 +0000

    Comprassin of value

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,3 +10,23 @@ console.log(typeof (valueInNumber));
 // "33" => 33
 // "33abs" => NaN
 // true => 1; false => 0
+
+// **************** Operation ***************
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="hello";
+let str2=" vineet";
+let str3 = str1 + str2;
+console.log(str3);
+ 
+console.log("1" +2);
+console.log(1 + "2");
