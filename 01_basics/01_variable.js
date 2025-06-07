cc3a7d4bc605dd14f60c65b941b5be2d0d8a0f93
vineet8057 <vineetkumar02102005@gmail.com>
2025-06-07 08:13:59 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 12873;
+let accountEmail ="vineet@google.com";
+var accountPassword = "12345";
+accountCity = " Bijnor";
+let accountState;
+// accountId =2 //note allowed
+
+accountEmail = "lalit@google.com";
+accountPassword = "564321";
+accountCity = "Noida";
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functioal scope
+*/
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3);// we are using nodejs, not browser
+
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// nul =>standalone value
+// symbol =>unique
+
+
+// object
+console.log(typeof "vineet");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
