099b71187f09b06844d39c0ee0da2a2e33f51d91
vineet8057 <vineetkumar02102005@gmail.com>
2025-06-07 10:11:09 +0000

    string methods

---
+++ 01_basics/06_string.js
@@ -0,0 +1,18 @@
+const name = "vineet"
+const rollNumber = 117
+
+// console.log(name + Lname);
+
+console.log(`Hello my name is ${name} and my last Roll Number is ${rollNumber}`);
+
+console.log(name.length);
+console.log(name.toUpperCase());
+
+
+console.log(name.charAt(2));
+console.log(name.indexOf('t'));
+
+
+
+
+
