9fb5dfb67753348835cae3fd95e101da7da9c221
vineet8057 <vineetkumar02102005@gmail.com>
2025-06-09 14:50:16 +0000

    json

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -23,6 +23,30 @@ const obj2 = {3: "a", 4: "b"}
 
 const obj3 = Object.assign({},obj1, obj2)
 // const obj3 = {obj1, obj2}
-console.log(obj3);
+// console.log(obj3);
 
+
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course;
+
+console.log(Instructor);
+
+// {
+//     "name": "vineet",
+//     "courseName": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
 
