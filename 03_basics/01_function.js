5ffe6349466f1e3a3bdcd77cf9ba6d82c8153168
vineet8057 <vineetkumar02102005@gmail.com>
2025-06-12 23:01:14 +0000

    Function with object and array

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -26,4 +26,22 @@ const result = addTwoNumber(3,7);
 function loginUserMessage(Username){
     return`${Username} just logged in`
 }
-console.log(loginUserMessage("vineet"))
+// console.log(loginUserMessage("vineet"))
+
+
+function calculateCartPrice(...num1){
+  return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 500,2000));
+
+const user = {
+  Username: "vineet",
+  price: 199
+}
+function handleObject(anyobject){
+  console.log(`Username is ${anyobject.Username} and price is ${anyobject.price}`);  
+  
+}
+
+handleObject(user);
